356a3ca6d245594ff458b0025a4a3f67e797d365
Juan Salazar <ferthelet@hotmail.com>
2022-04-29 16:49:25 -0500

    added random numbers library, and input control; and the idea for the version 2 =)

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,34 @@
-// my first game
-// also in https://replit.com/@JuanSalazar20/MyFirstGame#Main.java
+// my first game Random Points
+// earn points by guessing a number > mine
+// v2 will be guessing my number, the closer the more points earned
+// random cite: https://www.geeksforgeeks.org/generating-random-numbers-in-java/
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        Random rand = new Random();
+
         int ptMine = 0, ptYours = 0, numInput = 1, result = 0;
-        // fixed to 5 pending random number function
-        double numRandom = 5;
+        int randomNum;
 
         System.out.println("=============");
         System.out.println("Random points");
         System.out.println("=============");
         System.out.println("Earn points by guessing a number > mine (between 1-10 please)\n");
 
-        while(numInput != 0) {
-            // pending input control
+        while (numInput != 0) {
             System.out.print("Enter a number, 0 to exit: ");
+            randomNum = rand.nextInt(11);
             numInput = scanner.nextInt();
-            if (numInput != 0) {
-                result = numInput > numRandom ? ++ptYours : ++ptMine;
-                System.out.println("Yours: " + ptYours + "; Mine: " + ptMine);
+            if (numInput < 0 || numInput > 10) {
+                System.out.println("Between 1-10 please!");
+            } else if (numInput != 0) {
+                result = numInput > randomNum ? ++ptYours : ++ptMine;
+                System.out.println("Your points: " + ptYours + "; My number " + randomNum + ", my points: " + ptMine);
             }
         }
 
